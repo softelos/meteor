fec36dd5ee337ad27d72fdf83e99e68a9720fb75
Chakir Mrabet <cmrabet@softelos.com>
2016-02-15 08:36:23 -0500

    Fixed latency issue

--- client/templates/posts/post_add.js
+++ client/templates/posts/post_add.js
@@ -3,8 +3,7 @@ Template.postAdd.events({
 		e.preventDefault();
 		var post={
 			url:$(e.target).find('[name=url]').val(),
-			title:$(e.target).find('[name=title]').val(),
-			user_id:Meteor.userId
+			title:$(e.target).find('[name=title]').val()
 		};
 		Meteor.call('postAdd',post,function(error,result){
 			if(error) return alert(error.reason);
